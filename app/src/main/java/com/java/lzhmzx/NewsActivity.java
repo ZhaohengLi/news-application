88ac3044b1a1e16eb009e1049ae3c84328737e8e
ZhaohengLi <zhaohengli.thu@gmail.com>
2019-08-29 17:33:49 +0800

    Add NewsDataHelper class and reformat code.

--- app/src/main/java/com/java/lzhmzx/NewsActivity.java
+++ app/src/main/java/com/java/lzhmzx/NewsActivity.java
@@ -9,6 +9,7 @@ import com.google.android.material.snackbar.Snackbar;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.cardview.widget.CardView;
 
 import android.view.View;
 import android.widget.ImageView;
